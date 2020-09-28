2624ba65ced1a5aea1074ba3a228f8358b4013f4
Bi Mo Yong <bi.moyong@gmail.com>
2020-09-28 20:02:28 +0700

    new metadata obj

--- metadata/metadata.go
+++ metadata/metadata.go
@@ -6,6 +6,7 @@ import (
 
 // NewMetadata function
 func NewMetadata(md metadata.Metadata) (rst metadata.Metadata) {
+	rst = make(metadata.Metadata)
 	var keys = []string{"ID", "Alias", "Resource", "Domain", "Postback"}
 	for _, k := range keys {
 		if v, ok := md.Get(k); ok {
