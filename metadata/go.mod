242c1aa082fec6a8bb292c774324c176c29fca48
Bi Mo Yong <bi.moyong@gmail.com>
2020-09-28 19:54:16 +0700

    add package metadata

---
+++ metadata/go.mod
@@ -0,0 +1,5 @@
+module github.com/bimoyong/go-util/metadata
+
+go 1.14
+
+require github.com/micro/go-micro/v2 v2.9.1
---
+++ metadata/metadata.go
@@ -0,0 +1,17 @@
+package model
+
+import (
+	"github.com/micro/go-micro/v2/metadata"
+)
+
+// NewMetadata function
+func NewMetadata(md metadata.Metadata) (rst metadata.Metadata) {
+	var keys = []string{"ID", "Alias", "Resource", "Domain", "Postback"}
+	for _, k := range keys {
+		if v, ok := md.Get(k); ok {
+			rst.Set(k, v)
+		}
+	}
+
+	return
+}
